e972a1adddc029651068451e36159d8b3ef19d44
Ariel Gallardo <ariel.gallardo96@gmail.com>
2021-11-29 16:52:53 -0300

    autenticacion# modelado usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,10 @@
+namespace alkemy_challenge.Models
+{
+    public class Usuario
+    {
+        public long Id { get; set; }
+        public string Nombre { get; set; }
+        public string Password { get; set; }
+        public string Rol { get; set; }
+    }
+}
